b254bc1dd70cd4d5d98bb2746c78f9084e871b16
emiliojcheca <emilioj.checa@gmail.com>
2024-05-17 11:14:24 +0200

    delegación de rutas completada

--- exp_sharing_api/routes/api.js
+++ exp_sharing_api/routes/api.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 const apiUsersRouter = require('./api/users');
+const apiGroupsRouter = require('./api/groups');
+const apiExpensesRouter = require('./api/expenses');
+const apiInvitationsRouter = require('./api/invitations');
 
 router.use('/users', apiUsersRouter);
+router.use('/groups', apiGroupsRouter);
+router.use('/expenses', apiExpensesRouter);
+router.use('/invitations', apiInvitationsRouter);
 
 module.exports = router;
