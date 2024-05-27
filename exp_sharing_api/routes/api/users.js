633d5ee0e1560ba3464943a9d4cfb2718e14b025
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-05-27 06:44:49 +0200

    Resuelto conflicto con cors y eliminados bloques comentados

--- exp_sharing_api/routes/api/users.js
+++ exp_sharing_api/routes/api/users.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs')
 
-// const {createToken} = require ('../../common/JWTLogin')
-
 const { getAll, getAllbyGroup, getById, create, update, deleteById, getByMail } = require('../../models/user.model');
 
 /**
@@ -56,37 +54,6 @@ router.get("/:userId", async (req, res) => {
     res.json(error);
   }
 });
-
-
-// /**
-//  * POST /
-//  * 
-//  * Endpoint to create a new user.
-//  * Encrypts the user's password, saves the user in the database, and returns the created user.
-//  * 
-//  * @param {string} req.body.password - User's password to be encrypted.
-//  * @returns {Promise<void>} - Returns a JSON response with the new user or an error in case of failure.
-//  * 
-//  * @async
-//  */
-// router.post('/', async (req, res) => {
-//     // Password encryption
-//     req.body.password = bcrypt.hashSync(req.body.password)
-//     try {
-//       const [result] = await create(req.body);
-//       const newUserId = result.insertId
-//       const [[newUser]] = await getById(newUserId)
-
-//       let rememberMe = false
-//       res.json({
-//         success: "Registration successfull",
-//         token: createToken(newUser, rememberMe),
-//       });
-
-//     } catch (err) { 
-//         res.json(err);
-//     }
-// });
 
 router.put('/:id', async (req, res) => {
     try {
