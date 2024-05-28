f56ffa103cd4a8e9466b880dd9cede33f4ea18e8
Lorena Hurtado <hilorena.contact@gmail.com>
2024-05-28 17:00:33 +0100

    Group model - CRUD contra la tabla groups_app

--- exp_sharing_api/models/group.model.js
+++ exp_sharing_api/models/group.model.js
@@ -1,5 +1,21 @@
 // Queries contra la tabla groups_app:
 
 const getAll = () => {
-    return db.query('SELECT * FROM group_app');
+    return db.query('SELECT * FROM groups_app');
+}
+
+const getById = (groupId) => {
+    return db.query("SELECT * FROM groups_app WHERE id = ?", [groupId]);
+}
+
+const insert = ({ name, description, category }) => {
+    return db.query("INSERT INTO groups_app (name, description, category) VALUES (?, ?, ?)", [name, description, category]);
+}
+
+const updateById = (groupId, { name, description, category }) => {
+    return db.query("UPDATE groups_app SET name = ?, description = ?, category =? WHERE id = ?", [name, description, category, groupId]);
+}
+
+const deleteById = (groupId) => {
+    return db.query("UPDATE groups_app SET active = 0 where id = ?", [groupId]);
 }
