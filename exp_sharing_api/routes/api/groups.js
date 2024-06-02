97ebebcbf3a500f81f0f26786c79bf121b5b876b
emiliojcheca <emilioj.checa@gmail.com>
2024-06-02 12:52:14 +0200

    Cambio para obtener el usuario del token, y no como parámetro

--- exp_sharing_api/routes/api/groups.js
+++ exp_sharing_api/routes/api/groups.js
@@ -1,17 +1,19 @@
 const router = require('express').Router();
 const Group = require('../../models/group.model');
 
-router.get('/roles/:userId', async(req, res) => {
+/* Obtiene un objeto con dos arrays: grupos de admin y grupos como miembro (
+    el usuario lo obtiene del token a través del middelware checktoken */
+router.get('/roles', async(req, res) => {
     try {
         let roles = { admingroups: [], membergroups: []};
-        const [result] = await Group.getAllUserGroupsAsAdmin(req.params.userId);
+        const [result] = await Group.getAllUserGroupsAsAdmin(req.user.id);
         if (result[0]) 
         {
             const arrAdminGroups = result.map(obj => obj.id);
             roles.admingroups = arrAdminGroups
         }
 
-        const [result2] = await Group.getAllUserGroupsAsMember(req.params.userId);
+        const [result2] = await Group.getAllUserGroupsAsMember(req.user.id);
         if (result2[0]) 
         {
             roles.membergroups = result2.map(obj => obj.group_id);
