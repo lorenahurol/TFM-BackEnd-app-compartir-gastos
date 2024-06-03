cb3d69b2137b867c409144b689a8d9d51fdbd23a
Lorena Hurtado <hilorena.contact@gmail.com>
2024-06-03 17:23:41 +0100

    Crear invitaciones y actualizar el modelo

--- exp_sharing_api/models/invitation.model.js
+++ exp_sharing_api/models/invitation.model.js
@@ -1,26 +1,28 @@
-// Get all the existing (active) invitations
+// Get all the existing (active, pending) invitations
 const getAll = () => {
-    return db.query("SELECT * FROM invitations WHERE active = 1");
+    return db.query("SELECT * FROM invitations WHERE active = 1 AND accepted = 0");
 }
 
-const getById = (invitationId) => {
-    return db.query("SELECT * FROM invitations WHERE =? AND active = 1", [invitationId]);
+const getByUser = (userId) => {
+    return db.query("SELECT * FROM invitations WHERE user_id = ? AND (active = 1 OR accepted = 1)", [userId]);
 }
 
-/** Fecha se autogenera?? **/
-/** Se debe anadir email y mensaje a BD? */
-const insert = ({ groupId, userId, accepted, active }) => {
-    return db.query("INSERT INTO invitations (group_id, user_id, accepted, active) VALUES (?, ?, ?, ?)", [groupId, userId, accepted, active]);
+const getByGroupAndUser = (groupId, userId) => {
+    return db.query("SELECT * FROM invitations WHERE group_id = ? AND user_id = ?", [groupId, userId]);
 }
 
-//** No es necesario actualizar? */
+const insert = (invitationData) => {
+    return db.query("INSERT INTO invitations (group_id, user_id, accepted, active) VALUES (?, ?, ?, ?)", [invitationData.groupId, invitationData.userId, invitationData.accepted, invitationData.active]);
+}
+
+// User accepts or rejects invitation:
 
 // Delete invitation (deactivate invitation):
-const deactivateById = (invitationId) => {
+const deleteById = (invitationId) => {
     return db.query("UPDATE invitations SET active = 0 WHERE id = ?", [invitationId]);
 
 }
 
 module.exports = {
-    getAll, getById, insert, deactivateById
+    getAll, getByUser, getByGroupAndUser, insert, deleteById
 }
--- exp_sharing_api/routes/api/invitations.js
+++ exp_sharing_api/routes/api/invitations.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Invitation = require("../../models/invitation.model");
+const User = require("../../models/user.model");
 
 // Requests for /api/invitations:
 
@@ -16,6 +17,42 @@ router.get("/", async (req, res, next) => {
 
 // Create an invitation:
 router.post("/", async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const groupId = req.groupId // Gestionar en front
+
+        // Check that the fields are filled in:
+        if (!email) {
+            return res.status(400).json({ error: "Email is required" });
+        }
+
+        // Find the user_id through their email:
+        const [user] = await User.getByMail(email);
+        // Check they are registered:
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Check if the invitation exists:
+        const [invitationExists] = await Invitation.getByGroupAndUser(groupId, user.id);
+        if (invitationExists && (invitationExists.accepted || invitationExists.active)) {
+            return res.status(409).json({ error: "The user has already been invited" });
+        }
+
+        // Create the invitation:
+        const invitationData = {
+            groupId,
+            userId: user.id, // Usuario invitado
+            accepted: 0,
+            active: 1
+        }
+
+        const [result] = await Invitation.insert(invitationData);
+        res.json(result);
+
+    } catch (error) {
+        next(error);
+    }
 })
 
 
