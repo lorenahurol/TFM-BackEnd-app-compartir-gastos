515126055034074ccfb81692f3fedbbba06a04f6
Lorena Hurtado <hilorena.contact@gmail.com>
2024-06-06 15:47:44 +0100

    Validaciones para las invitaciones

--- exp_sharing_api/routes/api/invitations.js
+++ exp_sharing_api/routes/api/invitations.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 const Invitation = require("../../models/invitation.model");
-const User = require("../../models/user.model");
 
 // Requests for /api/invitations:
 
@@ -19,6 +18,20 @@ router.get("/", async (req, res, next) => {
 // Create an invitation (By Group Admin):
 router.post("/", async (req, res, next) => {
     try {
+
+        const { group_id, user_id, message } = req.body;
+
+        // Check that the fields are filled in:
+        if (!group_id || !user_id || !message) {
+            return res.json({ error: "Todos los campos son obligatorios" });
+        }
+
+        // Check if the invitation for the user already exists:
+        const pendingInvitation = await Invitation.getByGroupAndUser(group_id, user_id);
+            if (pendingInvitation[0].length > 0) {
+                return res.json({ error: "La invitación ya existe" });
+            }
+        
         const [result] = await Invitation.insert(req.body);
         res.json(result);
     } catch (error) {
@@ -30,42 +43,59 @@ router.post("/", async (req, res, next) => {
 router.put("/:invitationId/accept", async (req, res, next) => {
     try {
         const { invitationId } = req.params;
-        const [result] = await Invitation.updateStatus(invitationId, 1); // 1 === accepted
-            if (result.affectedRows === 0) {
-                return res.json({ error: "Pending invitation not found" });
+        const { user_id } = req.body;
+
+        // Check if the invitation exists for user_id
+        const [invitation] = await Invitation.getById(invitationId);
+
+            if (invitation.length === 0 || invitation[0].user_id !== user_id) {
+            return res.json({ error: "Invitación no encontrada" });
             }
-        res.json({ message: "Invitation accepted" });
+
+        // Accept invitation:
+        const [updateResult] = await Invitation.updateStatus(invitationId, 1); // 1 === accepted
+        res.json({ message: "Invitación aceptada", updateResult });
 
     } catch (error) {
+        console.error(error); // Imprimir el error
         next(error);
     }
 })
+
 
 // Reject an invitation (By invited user): URL: /api/invitation/invitationId/reject
 router.put("/:invitationId/reject", async (req, res, next) => {
     try {
         const { invitationId } = req.params;
-        const [result] = await Invitation.updateStatus(invitationId, 0); // 0 === rejected
-            if (result.affectedRows === 0) {
-                return res.json({ error: "Pending invitation not found" });
-            }
-        res.json({ message: "Invitation rejected" });
+        const { user_id } = req.body;
+
+        // Check if the invitation exists for user_id
+        const [invitation] = await Invitation.getById(invitationId);
+
+            if (invitation.length === 0 || invitation[0].user_id !== user_id) {
+            return res.json({ error: "Invitación no encontrada" });
+        }
+        
+        const [updateResult] = await Invitation.updateStatus(invitationId, 0); // 0 === rejected
+        res.json({ message: "Invitation rechazada", updateResult });
 
     } catch (error) {
         next(error);
     }
 })
 
-// Cancel an invitation (By Group Admin):
+// Deactivate an invitation (By Group Admin):
 router.delete("/:invitationId", async (req, res, next) => {
     try {
         const { invitationId } = req.params;
+
         const [result] = await Invitation.deleteById(invitationId);
             if (result.affectedRows === 0) {
-                return res.json({ error: "Invitation not found" });
+                return res.json({ error: "Invitación no encontrada" });
         }
-        res.json({ message: "Invitation deleted successfully" })
+        res.json({ message: "Invitación eliminada correctamente" })
     } catch (error) {
+        
         next(error);
     }
 })
