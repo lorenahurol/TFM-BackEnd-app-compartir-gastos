a7e979a75d66324ed8d30a1a6abdc958221c9f68
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-06-21 14:59:50 +0200

    Swagger doc

--- exp_sharing_api/routes/api/mails.js
+++ exp_sharing_api/routes/api/mails.js
@@ -6,31 +6,48 @@
  *       type: object
  *       required:
  *         - bcc
- *         - subject
- *         - template
+ *         - html
+ *         - selectedTemplate
+ *         - balance
+ *         - grouName
  *       properties:
  *         bcc:
+ *           type: array
+ *           items:
+ *             type: integer
+ *           description: An array of user IDs.
+ *         html:
  *           type: string
- *           description: An array of recipient user IDs.
- *         subject:
+ *           description: Alternative text for the email (overwrites templates).
+ *         selectedTemplate:
  *           type: string
- *           description: Subject of the email.
- *         template:
- *           type: int
  *           description: The selected email template to be used    
+ *         balance:
+ *           type: object
+ *           properties:
+ *             user_id: 
+ *               type: number
+ *               description: User id of members group (used when settling expenses)
+ *             group_id: 
+ *               type: number
+ *               description: Group id (used when settling expenses)
+ *             credit: 
+ *               type: number
+ *               description: Balance of a group member to be paid or received (used when settling expenses)
  *       example:
  *         bcc: [102, 5, 7]
- *         subject: Test group
- *         template: welcome
+ *         html: "<p><b>Test group</b> html example</p>"
+ *         selectedTemplate: "welcome"
+ *         balance: 
+ *           user_id: 20
+ *           group_id: 15
+ *           credit: -250.24
  */
-
 
 const router = require('express').Router();
 const { sendMail } = require('../../controllers/mails.controller');
 
-
 // Creacion nuevo email
-
 /**
  * @swagger
  * tags:
@@ -51,10 +68,8 @@ const { sendMail } = require('../../controllers/mails.controller');
  *         description: The emails have been sent.
  *       500:
  *         description: Some server error
- *
  */
+
 router.post('/', sendMail);
 
 module.exports = router;
-
-
--- exp_sharing_api/routes/api/register.js
+++ exp_sharing_api/routes/api/register.js
@@ -1,8 +1,126 @@
 const router = require('express').Router();
 const { existUsername, createNewUser, existMail } = require('../../controllers/register.controller');
 
+  /**
+ * @swagger
+ * tags:
+ *   name: Register
+ *   description: The Registration managing API
+ * /api/register/checkUsername/{username}:
+ *   get:
+ *     summary: Checks if the selected username already exists in database
+ *     tags: [Register]
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Username
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 exists:
+ *                   type: boolean
+ *                   description: True if the username exists
+ *       500:
+ *         description: Server Error
+ */
 router.get("/checkUsername/:username", existUsername);
+
+  /**
+ * @swagger
+ * tags:
+ *   name: Register
+ *   description: The Registration managing API
+ * /api/register/checkMail/{mail}:
+ *   get:
+ *     summary: Checks if the selected email already exists in database and the corresponding user is active
+ *     tags: [Register]
+ *     parameters:
+ *       - in: path
+ *         name: mail
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: email
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 active:
+ *                   type: boolean
+ *                   description: True if the email exists and the user is active, false if the email exists but the user has been deactivated, null is the email does not exist
+ *       500:
+ *         description: Server Error
+ */
 router.get("/checkMail/:mail", existMail);
+
+/**
+ * @swagger
+ * tags:
+ *   name: Register
+ *   description: The Registration managing API
+ * /api/register:
+ *   post:
+ *     summary: Creates a new user
+ *     tags: [Register]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               mail:
+ *                 type: string
+ *                 description: User's email
+ *               username:
+ *                 type: string
+ *                 description: User's selected username
+ *               firstname:
+ *                 type: string
+ *                 description: User's first name
+ *               lastname:
+ *                 type: string
+ *                 description: User's last name
+ *               phone:
+ *                 type: string
+ *                 description: Telephone
+ *               image:
+ *                 type: string
+ *                 description: User's image
+ *               password:
+ *                 type: string
+ *                 description: User's encrypted password
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: string
+ *                   description: Successfull registration message
+ *                 token:
+ *                   type: string
+ *                   description: encripted user token
+ *       400:
+ *         description: Error when updating information
+ *       500:
+ *         description: Server Error
+ */
 router.post('/', createNewUser);
 
 module.exports = router;
